c9df0c411d9aefb3363ed15fcd7857db2140d044
Saga690 <master.ayush.2003@gmail.com>
2025-07-06 06:30:38 +0530

    fixed issue

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         <CustomButton
           btnType="button"
           title={address ? "Create a campaign" : "Connect"}
-          styles={`address ? bg-[#1dc071] : bg-[#8c6dfd]`}
+          styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
           handleClick={() => {
             if (address) {
               navigate("/create-campaign");
