332ddc81b0583e99050ce217f6e6c0e81e7e7a1d
Saga690 <master.ayush.2003@gmail.com>
2025-07-06 05:51:59 +0530

    added sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,13 +1,61 @@
-import React, {useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import {logo, sun} from '../assets';
-import {navlinks} from '../constants';
+import { logo, sun } from "../assets";
+import { navlinks } from "../constants";
+
+const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => {
+  return (
+    <div
+      className={`w-[48px] h-[48px] rounded-[10px] ${
+        isActive && isActive === name ? "bg-[#2c2f32]" : "bg-[#1c1f22]"
+      } flex justify-center items-center ${styles} ${
+        disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"
+      }`}
+      onClick={handleClick}
+    >
+      {!isActive ? (
+        <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+      ) : (
+        <img
+          src={imgUrl}
+          alt="fund_logo"
+          className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+        />
+      )}
+    </div>
+  );
+};
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const [active, setActive] = useState("dashboard");
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="flex flex-col justify-between items-center sticky top-5 h-[93vh]">
+      <Link to="/">
+        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+      </Link>
+      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] w-[76px] rounded-[20px] py-4 mt-12">
+        <div className="flex flex-col justify-center items-center gap-3">
+          {navlinks.map((link) => (
+            <Icon
+              key={link.name}
+              {...link}
+              isActive={active}
+              handleClick={() => {
+                if (!link.disabled) {
+                  setActive(link.name);
+                  navigate(link.link);
+                }
+              }}
+            />
+          ))}
+        </div>
 
-export default Sidebar
+        <Icon styles="bg-[#1c1c24] shadow-secondary cursor-pointer" imgUrl={sun} />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
